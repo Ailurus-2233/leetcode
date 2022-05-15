e79fe9624ae129bb20debc15ab5bf273e73aed97
Ailurus <Ailurus2233@outlook.com>
2022-05-15 12:47:53 +0800

    2022/05/15

--- test.go
+++ test.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	s := "abcdefg"
-	for i, v := range s {
-		fmt.Println(byte(v))
-		fmt.Println(s[i])
-	}
+	// s := "abcdefg"
+	// for i, v := range s {
+	// 	fmt.Println(byte(v))
+	// 	fmt.Println(s[i])
+	// }
+	ans := make([]int, 0, 10)
+	ans = append(ans, 1)
+	fmt.Println(ans)
 }
---
+++ 题库/812.最大三角形面积.go
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=812 lang=golang
+ *
+ * [812] 最大三角形面积
+ */
+package main
+
+import "math"
+
+// @lc code=start
+func triangleArea(x1, y1, x2, y2, x3, y3 int) float64 {
+	return math.Abs(float64(x1*y2+x2*y3+x3*y1-x1*y3-x2*y1-x3*y2)) / 2
+}
+
+func largestTriangleArea(points [][]int) (ans float64) {
+	for i, p := range points {
+		for j, q := range points[:i] {
+			for _, r := range points[:j] {
+				ans = math.Max(ans, triangleArea(p[0], p[1], q[0], q[1], r[0], r[1]))
+			}
+		}
+	}
+	return
+}
+
+// @lc code=end
