9c61e5fc0a76d9f803ee5af3b13acb25ebba3c98
Ailurus/wzy <ailurus2233@outook.com>
2022-09-01 21:20:47 +0800

    2022/09/01

--- Solution.java
+++ Solution.java
@@ -1,12 +1,18 @@
-import java.util.Arrays;
-import java.util.HashSet;
-
+/**
+ * Solution
+ */
 public class Solution {
 
     public static void main(String[] args) {
-        Integer[] a = {1, 2, 2, 3, 4};
-        HashSet<Integer> hs = new HashSet<Integer>(Arrays.asList(a));
-        Object[] b =  hs.toArray();
-        System.out.println(Arrays.toString(b));
+        System.out.println(func("abs"));
+    }
+
+    public static String func(String s) {
+        char[] chars = new char[s.length()];
+        int index = s.length();
+        for (char c: s.toCharArray()) {
+            chars[--index] = c;
+        }
+        return String.valueOf(chars);
     }
 }
